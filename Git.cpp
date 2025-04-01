0fb3b07fcf07c73e1620ef10e0abb00988a92e8b
alice <a.chakileva@icloud.com>
2025-04-02 00:45:00 +0600

    Add project files.

---
+++ Git.cpp
@@ -0,0 +1,7 @@
+#include"Git.h"
+#include <iostream>
+
+void Git::HiMatt()
+{
+	std::cout << "maybe some changes?" << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include"Git.h"
+
+int main() {
+	std::cout << "hi its my project" << std::endl;
+	std::cout << "i dont wanna do it )" << std::endl;
+	std::cout << "u want some changes ?" << std::endl;
+}
