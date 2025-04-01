a0b1f10cc75e00a6ad6dcdaddcaf42758221b508
alice <a.chakileva@icloud.com>
2025-04-02 00:49:42 +0600

    Edited first lines in all three files

--- Git.cpp
+++ Git.cpp
@@ -4,4 +4,6 @@
 void Git::HiMatt()
 {
 	std::cout << "maybe some changes?" << std::endl;
+	std::cout << "my change!" << std::endl;
+
 }
--- main.cpp
+++ main.cpp
@@ -5,4 +5,5 @@ int main() {
 	std::cout << "hi its my project" << std::endl;
 	std::cout << "i dont wanna do it )" << std::endl;
 	std::cout << "u want some changes ?" << std::endl;
+	std::cout << "this is my FIRST change!" << std::endl;
 }
